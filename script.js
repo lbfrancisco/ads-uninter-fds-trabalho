c0ba050f92b4f56593355525f4d961cb93ebb057
Lucas Francisco <lucasbarroso318@gmail.com>
2025-04-13 19:28:31 -0300

    fix: add await to fetch request

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ surpriseButton.addEventListener("click", getYourMessage)
 let phrase = {};
 
 async function getYourMessage() {
-  fetch('https://moraislucas.github.io/MeMotive/phrases.json')
+  await fetch('https://moraislucas.github.io/MeMotive/phrases.json')
   .then(response => {
     if (!response.ok) {
       throw new Error(`Erro na requisição: ${response.statusText}`);
